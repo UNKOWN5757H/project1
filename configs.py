e0b332682670a2896abbee0e247a006940e0acb7
UNKOWN5757H <65911268+UNKOWN5757H@users.noreply.github.com>
2025-10-04 21:35:21 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,11 +1,11 @@
 from os import getenv
 
 class Config:
-    API_ID = int(getenv("API_ID", "1313697"))
-    API_HASH = getenv("API_HASH", "9f1f1ed9bb3585823c77e1953d3b102d")
-    BOT_TOKEN = getenv("BOT_TOKEN", "7969568367:AAE8SnlYzjuD3z6IrX42jy9evVIqVGFchxg")
+    API_ID = int(getenv("API_ID", ""))
+    API_HASH = getenv("API_HASH", "")
+    BOT_TOKEN = getenv("BOT_TOKEN", "")
     # Admin Or Owner Id(s)
     SUDO = list(map(int, getenv("SUDO", "2098589219").split()))
-    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://Autof1:Autof01@autof1.zza5eft.mongodb.net/?retryWrites=true&w=majority&appName=Autof1")
+    MONGO_URI = getenv("MONGO_URI", "")
 
 cfg = Config()
